830a83e6feae917b95daa45bb1007b57fffae640
Anara Orazalina <orazalina.anara@inbox.ru>
2021-02-26 18:15:35 +0600

    added

---
+++ src/main/java/ru/netology/domain/Vk.java
@@ -0,0 +1,29 @@
+package ru.netology.domain;
+
+public class Vk {
+    private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private CommentsInfo commentsInfo;
+    private CopyrightInfo copyright;
+    private LikesInfo likesInfo;
+    private RepostsInfo reposts;
+    private int views;
+    private String postType;
+    private GeoInfo geo;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int posrponedId;
+
+}
