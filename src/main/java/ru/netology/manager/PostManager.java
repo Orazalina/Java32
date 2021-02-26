20e8d78d6afdb4f6221ba7d288eb4c0eb7cf3c4d
Anara Orazalina <orazalina.anara@inbox.ru>
2021-02-26 22:16:58 +0600

    meneger added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post posts;
+    public String generatePage(){
+        return null;
+    }
+    public Post[] search (int id, int ounerId){
+        return null;
+    }
+    public void delete (int id){
+    }
+}
