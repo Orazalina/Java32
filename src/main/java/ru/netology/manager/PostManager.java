a3602e60ace09baa0f471229bf8be2edfbd0b1ac
Anara Orazalina <orazalina.anara@inbox.ru>
2021-02-28 22:47:36 +0600

    fixed code

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,12 +4,15 @@ import ru.netology.domain.Post;
 
 public class PostManager {
     private Post[] posts;
+
     public Post[] getId() {
         return null;
     }
-    public Post[] search (int id, int ounerId){
+
+    public Post[] search(int ounerId, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
-    public void delete (int id){
+
+    public void delete(int ownerId, int postId) {
     }
 }
